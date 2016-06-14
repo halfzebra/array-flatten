2322d9ce7a5b306392d877637382b198ac411c39
Drew11 <drew.naumenko@gmail.com>
2016-06-14 08:38:13 +0300

    no jquery

--- map.js
+++ map.js
@@ -1,11 +1,9 @@
-var input = [1, [2, [3, [4]]],[5]];
-var expectedOutput = [1, 2, 3, 4, 5];
+module.exports = function flatten (input) {
+
+
 var output = [];
 input.map(function recurs(n) {
-    Array.isArray(n) ? output.concat(n.map(recurs)): output.push(n);
+    Array.isArray(n) ? output.concat(n.map(recurs)) : output.push(n);
 
-
-
-});
-
-console.log(output);
+    return output;
+})};
