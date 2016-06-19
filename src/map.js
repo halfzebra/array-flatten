ad21ad30cd4f14ea6dc0fe7ae5b55c74eb923c45
Eduard Kyvenko <eduard@dinero.dk>
2016-06-19 18:54:58 +0200

    Converted to ES2015 modules

--- src/map.js
+++ src/map.js
@@ -1,12 +1,11 @@
-module.exports = function flatten(input) {
+export default function flatten (input) {
     var result = [];
-    input.map(function recurs(n) {
-        if (Array.isArray(n))
-            result.concat(n.map(recurs));
-        else
-            result.push(n);
-
-
+    input.map(function recurs (n) {
+        if (Array.isArray(n)) {
+            result.concat(n.map(recurs))
+        } else {
+            result.push(n)
+        }
     });
     return result;
 };
