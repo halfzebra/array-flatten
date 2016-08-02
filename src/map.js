e10dfaf3f04679b23bf3a22714a6286ad908183f
Eduard Kyvenko <eduard@dinero.dk>
2016-08-02 14:43:08 +0200

    Added .editorconfig

--- src/map.js
+++ src/map.js
@@ -1,13 +1,13 @@
 export default function flatten (input) {
-    let result = [];
-    input.map(function recurs (value) {
-        if (Array.isArray(value)) {
-            result.concat(value.map(recurs))
-        } else {
-            result.push(value)
-        }
-    });
-    return result;
+  let result = [];
+  input.map(function recurs (value) {
+    if (Array.isArray(value)) {
+      result.concat(value.map(recurs))
+    } else {
+      result.push(value)
+    }
+  });
+  return result;
 };
 
 
